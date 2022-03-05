1b424ad469f76b9ef70258b5f3f931b237342795
ofoeduchisom <ofoeduchisomd@gmail.com>
2022-03-05 09:53:35 +0000

    initial project

---
+++ src/env.js
@@ -0,0 +1,3 @@
+export default {
+    apikey: "4f0fa563"
+}
---
+++ src/views/Home.vue
@@ -0,0 +1,221 @@
+<template>
+  <div class="home">
+    <div class="feature-card">
+      <router-link to="/movie/tt0489591">
+        <img
+          src="https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg"
+          alt="The simpsons Poster"
+          class="featured-img"
+        />
+        <div class="detail">
+          <h3>The Simpsons</h3>
+          <p>
+            This is an animated sitcom about the antics of a dysfunctional
+            family. Homer is the oafish unhealthy beer loving father, Marge is
+            the hardworking homemaker wife, Bart is the perpetual ten-year-old
+            underachiever (and proud of it), Lisa is the unappreciated
+            eight-year-old genius, and Maggie is the cute, pacifier loving
+            silent infant.
+          </p>
+        </div>
+      </router-link>
+    </div>
+    <form @submit.prevent="SearchMovies()" class="search-box">
+      <input
+        type="text"
+        placeholder="what are you looking for?"
+        v-model="search"
+      />
+      <input type="submit" value="Search" />
+    </form>
+    <div class="movies-list">
+      <div class="movie" v-for="movie in movies" :key="movie.imdbID">
+        <router-link :to="'/movie/' + movie.imdbID" class="movie-link">
+          <div class="product-image">
+            <img :src="movie.Poster" alt="Movie Poster" />
+            <div class="type">{{ movie.Type }}</div>
+          </div>
+          <div class="detail">
+            <p class="year">{{ movie.Year }}</p>
+            <h3>{{ movie.Title }}</h3>
+          </div>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import env from "@/env.js";
+
+export default {
+  setup() {
+    const search = ref("");
+    const movies = ref([]);
+
+    const SearchMovies = () => {
+      if (search.value != "") {
+        fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
+          .then((response) => response.json())
+          .then((data) => {
+            movies.value = data.Search;
+            search.value = "";
+          });
+      }
+    };
+
+    return {
+      search,
+      movies,
+      SearchMovies,
+    };
+  },
+};
+</script>
+
+<style>
+.home {
+  display: block;
+}
+.feature-card {
+  position: relative;
+}
+
+.featured-img {
+  display: block;
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
+  position: relative;
+  z-index: 0;
+}
+
+detail {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 16px;
+  z-index: 1;
+}
+
+h3 {
+  color: #fff;
+  margin-bottom: 16px;
+}
+
+p {
+  color: #fff;
+}
+
+.search-box {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 16px;
+}
+input {
+  display: block;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+}
+
+input[type="text"] {
+  width: 100%;
+  color: #fff;
+  background-color: #496583;
+  font-size: 20px;
+  padding: 10px 16px;
+  border-radius: 8px;
+  margin-bottom: 15px;
+  transition: 0.4s;
+}
+
+::placeholder {
+  color: #f3f3f3;
+}
+
+:focus {
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.2);
+}
+
+input[type="submit"] {
+  width: 100%;
+  max-width: 300px;
+  background-color: #42b883;
+  padding: 16px;
+  border-radius: 8px;
+  color: #fff;
+  font-size: 20px;
+  text-transform: uppercase;
+  transition: 0.4s;
+}
+
+:active {
+  background-color: #3b8070;
+}
+
+.movies-list {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0px 8px;
+}
+
+.movie {
+  max-width: 50%;
+  flex: 1 1 50%;
+  padding: 16px 8px;
+}
+
+.movie-link {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.product-image {
+  position: relative;
+  display: block;
+}
+
+img {
+  display: block;
+  width: 100%;
+  height: 275px;
+  object-fit: cover;
+}
+
+.type {
+  position: absolute;
+  padding: 8px 16px;
+  background-color: #42b883;
+  color: #fff;
+  bottom: 16px;
+  left: 0px;
+  text-transform: capitalize;
+}
+
+.detail {
+  background-color: #496583;
+  padding: 16px 8px;
+  flex: 1 1 100%;
+  border-radius: 0px 0px 8px 8px;
+}
+
+year {
+  color: #aaa;
+  font-size: 14px;
+}
+
+h3 {
+  color: #fff;
+  font-weight: 600;
+  font-size: 18px;
+}
+</style>
+
